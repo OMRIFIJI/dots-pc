bec074b3cfcae4ca4879e189fa37c0de6b9d381c
OMRIFIJI <amirkarimov46@gmail.com>
2024-02-07 22:17:53 +0300

    Clean initial commit

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    export XDG_RUNTIME_DIR="/run/user/1000"
-end
-
-if status is-login
-    source ~/.local/bin/wrappedhl
-end
